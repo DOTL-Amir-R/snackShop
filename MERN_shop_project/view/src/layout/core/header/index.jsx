27b6ada1fb10b457f57ee013399822e8fe0c6622
DOTL-Amir-R <raeisi.amirrr@gmail.com>
2023-01-29 17:15:53 +0330

    added branch for header and footer

--- MERN_shop_project/view/src/layout/core/header/index.jsx
+++ MERN_shop_project/view/src/layout/core/header/index.jsx
@@ -1,26 +1,41 @@
-import {svgObject} from '../../../assets/svg'
-import { FaGift , FaQuestionCircle ,  FaUser ,FaChevronDown } from 'react-icons/fa'
-
+import { svgObject } from '../../../assets/svg';
+import {
+    FaGift,
+    FaQuestionCircle,
+    FaUser,
+    FaChevronDown,
+} from 'react-icons/fa';
+import './index.css';
 
 export function Header() {
     return (
-        <div className="flex justify-between">
-            <div className="flex gap-5">
-                <div>ARDOTL</div>
-                <div>subscribe</div>
-                <div>ARDOTL</div>
+        <nav className="p-2.5 flex justify-between font-size-18 font-weight-400 container">
+            <div className="flex gap-5 items-center">
+                <div className="font-weight-700 font-size-27 p-2.5">ARMHA</div>
                 <div>subscribe</div>
                 <div>shop</div>
                 <div>about</div>
                 <div>gifts</div>
                 <div>for business</div>
-                <div className='flex'><FaGift size='1.2rem' />rewards</div>
+                <div className="flex items-center gap-1">
+                    <FaGift size="1.2rem" />
+                    rewards
+                </div>
             </div>
-            <div className="flex gap-5">
-                <div className='flex'><FaQuestionCircle size='1.2rem' />help</div>
-                <div className='flex'> {svgObject.unitedKingdom} uk<FaChevronDown size='1.2rem'/></div>
-                <div className='flex'> <FaUser  size='1.2rem' />log in</div>
+            <div className="flex gap-5 items-center">
+                <div className="flex items-center gap-1">
+                    <FaQuestionCircle size="1.2rem" />
+                    help
+                </div>
+                <div className="flex items-center gap-1">
+                    <img src='https://cdn.shopify.com/s/files/1/0064/7652/9737/files/united-kingdom.svg?v=1584632335' width='15px' height='15px'/> uk
+                    <FaChevronDown size="1.2rem" />
+                </div>
+                <div className="flex items-center gap-1">
+                    <FaUser size="1.2rem" />
+                    log in
+                </div>
             </div>
-        </div>
+        </nav>
     );
 }
