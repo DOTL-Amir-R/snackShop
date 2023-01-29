63358280e5a4fe29278867c5e5aa039778a1e498
DOTL-Amir-R <raeisi.amirrr@gmail.com>
2023-01-29 21:11:06 +0330

    added header and footer

--- MERN_shop_project/view/src/layout/core/header/index.jsx
+++ MERN_shop_project/view/src/layout/core/header/index.jsx
@@ -1,17 +1,17 @@
-import { svgObject } from '../../../assets/svg';
+// import { svgObject } from '../../../assets/svg';
 import {
     FaGift,
     FaQuestionCircle,
     FaUser,
     FaChevronDown,
 } from 'react-icons/fa';
-import './index.css';
+import '../../global/index.css';
 
 export function Header() {
     return (
-        <nav className="p-2.5 flex justify-between font-size-18 font-weight-400 container">
+        <nav className="padd-top-bot-22  flex justify-between font-size-18 font-weight-400 container">
             <div className="flex gap-5 items-center">
-                <div className="font-weight-700 font-size-27 p-2.5">ARMHA</div>
+                <div className="font-weight-700 font-size-27 py-2.5">ARMHA</div>
                 <div>subscribe</div>
                 <div>shop</div>
                 <div>about</div>
@@ -29,7 +29,7 @@ export function Header() {
                 </div>
                 <div className="flex items-center gap-1">
                     <img src='https://cdn.shopify.com/s/files/1/0064/7652/9737/files/united-kingdom.svg?v=1584632335' width='15px' height='15px'/> uk
-                    <FaChevronDown size="1.2rem" />
+                    <FaChevronDown size="0.75rem" />
                 </div>
                 <div className="flex items-center gap-1">
                     <FaUser size="1.2rem" />
