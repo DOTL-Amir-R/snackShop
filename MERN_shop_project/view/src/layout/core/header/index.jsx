e3780c6294e3af27cf0750f32dc34f0ef2fad70f
DOTL-Amir-R <raeisi.amirrr@gmail.com>
2023-01-30 18:45:40 +0330

    fixedHeader

--- MERN_shop_project/view/src/layout/core/header/index.jsx
+++ MERN_shop_project/view/src/layout/core/header/index.jsx
@@ -6,6 +6,7 @@ import {
     FaChevronDown,
 } from 'react-icons/fa';
 import '../../global/index.css';
+import './index.css';
 
 export function Header() {
     return (
@@ -18,7 +19,7 @@ export function Header() {
                 <div>gifts</div>
                 <div>for business</div>
                 <div className="flex items-center gap-1">
-                    <FaGift size="1.2rem" />
+                    <FaGift className="color-red-fa6161" size="1.2rem" />
                     rewards
                 </div>
             </div>
@@ -27,9 +28,49 @@ export function Header() {
                     <FaQuestionCircle size="1.2rem" />
                     help
                 </div>
-                <div className="flex items-center gap-1">
-                    <img src='https://cdn.shopify.com/s/files/1/0064/7652/9737/files/united-kingdom.svg?v=1584632335' width='15px' height='15px'/> uk
-                    <FaChevronDown size="0.75rem" />
+                <div className="flex items-center flex-col position-relative">
+                    <div className="flex items-center gap-1 ">
+                        <img
+                            src="https://cdn.shopify.com/s/files/1/0064/7652/9737/files/united-kingdom.svg?v=1584632335"
+                            width="15px"
+                            height="15px"
+                        />
+                        uk
+                        <FaChevronDown size="0.75rem" />
+                    </div>
+                    <div
+                        style={{
+                            position: 'absolute',
+                            right: '45px',
+                            bottom: '-84px',
+                            display: 'none',
+                        }}
+                    >
+                        <div className="flex items-center gap-1">
+                            <img
+                                src="https://cdn.shopify.com/s/files/1/0064/7652/9737/files/ireland.svg?v=1584632334"
+                                width="15px"
+                                height="15px"
+                            />{' '}
+                            irland
+                        </div>
+                        <div className="flex items-center gap-1">
+                            <img
+                                src="https://cdn.shopify.com/s/files/1/0064/7652/9737/files/netherlands.svg?v=1584632334"
+                                width="15px"
+                                height="15px"
+                            />{' '}
+                            nederland
+                        </div>
+                        <div className="flex items-center gap-1">
+                            <img
+                                src="https://cdn.shopify.com/s/files/1/0558/6118/2631/files/germany.svg?v=1630071157"
+                                width="15px"
+                                height="15px"
+                            />{' '}
+                            germany
+                        </div>
+                    </div>
                 </div>
                 <div className="flex items-center gap-1">
                     <FaUser size="1.2rem" />
