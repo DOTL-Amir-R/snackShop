fa4da4c40fa86a08b69bc5cb3bf1b6b4f95f38f0
DOTL-Amir-R <raeisi.amirrr@gmail.com>
2023-02-07 19:41:35 +0330

    RegistrationEditV03(AddedTailWindPackage)

--- MERN_shop_project/view/src/page/signup/index.jsx
+++ MERN_shop_project/view/src/page/signup/index.jsx
@@ -1,10 +1,11 @@
+import { Forms } from "./forms";
 import { SignupHeader } from "./header";
 
 export function SignUp() {
     return(
         <div>
             <SignupHeader/>
-            <div>hello</div>
+            <Forms/>
         </div>
     )
 }
