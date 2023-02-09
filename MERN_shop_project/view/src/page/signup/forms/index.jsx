c16688af4a4dd9c4c8862cba7945966540a5fbf7
DOTL-Amir-R <raeisi.amirrr@gmail.com>
2023-02-09 20:33:19 +0330

    ICantContinnue!PlsHelp!RegestrationEditV07IThink

--- MERN_shop_project/view/src/page/signup/forms/index.jsx
+++ MERN_shop_project/view/src/page/signup/forms/index.jsx
@@ -1,20 +1,21 @@
 import { Input } from '@material-tailwind/react';
 import { FloatLabelInput } from '../../../components/FloatLableInput';
+import './index.css'
 
 export function Forms() {
     return (
-        <div className="flex flex-col width-58-percent  ">
+        <div className="flex flex-col width-58-percent  pl-4">
             <div className="text-2xl font-medium">tell us about you…</div>
             <div className="m-10 flex flex-col">
-                <div>
+                <div className='text-lg my-2.5 color-black-444'>
                     Let’s set up your account, so you can tailor your
                     preferences and we can send you the perfect box!
                 </div>
                 <form>
-                    <FloatLabelInput type='email' labelText='email' classNameInput='border-none' />
-                    <FloatLabelInput type='email' labelText='email' classNameInput='border-none' />
-                    <FloatLabelInput type='email' labelText='email' classNameInput='border-none' />
-                    <FloatLabelInput type='email' labelText='email' classNameInput='border-none' />
+                        <FloatLabelInput type='text' labelText='first name' classNameLabel='text-lg color-black-444 input-label-classes ' classNameInput='border-none input-classes' />
+                        <FloatLabelInput type='text' labelText='last name' classNameLabel='text-lg color-black-444 input-label-classes ' classNameInput='border-none input-classes' />
+                        <FloatLabelInput type='email' labelText='email' classNameLabel='text-lg color-black-444 input-label-classes ' classNameInput='border-none input-classes' />
+                        <FloatLabelInput type='password' labelText='password' classNameLabel='text-lg color-black-444 input-label-classes ' classNameInput='border-none input-classes' />
                 </form>
 
                 <div>do you have any allergies?</div>
