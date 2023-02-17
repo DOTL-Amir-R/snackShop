f2eca129bb943645264d10851a779e384bbb8c61
mohammadHossein712 <mohammadhossdinahmadian@gmail.com>
2023-02-17 22:04:35 +0330

    product cards part complited

---
+++ MERN_shop_project/view/src/components/shared/ProductCard/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function ProductCard({name , picture , price}) {
+    return (
+        <div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <a href="#">
+                <img className=" rounded-t-lg" src={picture} alt="product image" />
+            </a>
+            <div className="px-5 pb-5">
+                <a href="#">
+                    <h5 className="text-xl font-semibold pt-3 tracking-tight text-gray-900 dark:text-white">{name}</h5>
+                </a>
+                <div className="flex items-center mt-2.5 mb-5">
+                    <svg ariaHidden="true" className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>First star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                    <svg ariaHidden="true" className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Second star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                    <svg ariaHidden="true" className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Third star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                    <svg ariaHidden="true" className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fourth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                    <svg ariaHidden="true" className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fifth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                    <span className=" text-gray-800 text-xs font-semibold mr-2  py-0.5 rounded dark:text-blue-800 ">2 reviews</span>
+                </div>
+                <div className="flex items-center justify-between">
+                    <span className="text-lg text-gray-900 dark:text-white">{`$${price}`}</span>
+                    <button className="text-white bg-green-500 hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-3xl text-sm px-5 py-2.5 text-center">add</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProductCard
--- MERN_shop_project/view/src/page/shop/index.jsx
+++ MERN_shop_project/view/src/page/shop/index.jsx
@@ -1,16 +1,32 @@
 import Breadcrumb from '../../components/shared/breadcrumb'
+import ProductCard from '../../components/shared/ProductCard'
 import React from 'react'
 import grazeNewHero from "../../assets/img/graze_new_hero.webp"
+import PunnetFlapjackLimitedEdition from "../../assets/img/Punnet-Flapjack-Limited-Edition.webp"
 
 function Shop() {
     return (
         <main className='bg-neutral-50'>
             <h5 className='bg-pink-500 text-center text-white py-2.5 text-xl'><b>You'll get your first box half price</b> (usually £4.99) </h5>
-            <Breadcrumb/>
+            <Breadcrumb />
             <div style={{ backgroundImage: `URL(${grazeNewHero})` }} className='py-20'>
                 <h1 className='text-center text-slate-50 text-4xl font-bold mb-2'>shop now</h1>
                 <p className='text-center text-slate-50 text-2xl font-semibold'>choose your sutable graze box</p>
             </div>
+            <section className='grid grid-cols-4 gap-5 container py-8'>
+                <ProductCard name="mince pie flapjack multipack" price="599" picture={PunnetFlapjackLimitedEdition} />
+                <ProductCard name="mince pie flapjack multipack" price="599" picture={PunnetFlapjackLimitedEdition} />
+                <ProductCard name="mince pie flapjack multipack" price="599" picture={PunnetFlapjackLimitedEdition} />
+                <ProductCard name="mince pie flapjack multipack" price="599" picture={PunnetFlapjackLimitedEdition} />
+                <ProductCard name="mince pie flapjack multipack" price="599" picture={PunnetFlapjackLimitedEdition} />
+                <ProductCard name="mince pie flapjack multipack" price="599" picture={PunnetFlapjackLimitedEdition} />
+                <ProductCard name="mince pie flapjack multipack" price="599" picture={PunnetFlapjackLimitedEdition} />
+                <ProductCard name="mince pie flapjack multipack" price="599" picture={PunnetFlapjackLimitedEdition} />
+                <ProductCard name="mince pie flapjack multipack" price="599" picture={PunnetFlapjackLimitedEdition} />
+                <ProductCard name="mince pie flapjack multipack" price="599" picture={PunnetFlapjackLimitedEdition} />
+                <ProductCard name="mince pie flapjack multipack" price="599" picture={PunnetFlapjackLimitedEdition} />
+                <ProductCard name="mince pie flapjack multipack" price="599" picture={PunnetFlapjackLimitedEdition} />
+            </section>
         </main>
     )
 }
