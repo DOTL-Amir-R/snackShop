0fa8367ddc97dd93cbab5590e193a8ec47f141b3
mohammadHossein712 <mohammadhossdinahmadian@gmail.com>
2023-02-18 13:43:46 +0330

    Shop page ui complited

--- MERN_shop_project/view/src/components/shared/ProductCard/index.jsx
+++ MERN_shop_project/view/src/components/shared/ProductCard/index.jsx
@@ -16,10 +16,10 @@ function ProductCard({name , picture , price}) {
                     <svg ariaHidden="true" className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Third star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
                     <svg ariaHidden="true" className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fourth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
                     <svg ariaHidden="true" className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fifth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-                    <span className=" text-gray-800 text-xs font-semibold mr-2  py-0.5 rounded dark:text-blue-800 ">2 reviews</span>
+                    <span className=" text-gray-800 text-base font-semibold mr-2  py-0.5 rounded ">2 reviews</span>
                 </div>
                 <div className="flex items-center justify-between">
-                    <span className="text-lg text-gray-900 dark:text-white">{`$${price}`}</span>
+                    <span className="text-lg text-gray-900">{`$${price}`}</span>
                     <button className="text-white bg-green-500 hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-3xl text-sm px-5 py-2.5 text-center">add</button>
                 </div>
             </div>
--- MERN_shop_project/view/src/page/shop/index.jsx
+++ MERN_shop_project/view/src/page/shop/index.jsx
@@ -13,6 +13,13 @@ function Shop() {
                 <h1 className='text-center text-slate-50 text-4xl font-bold mb-2'>shop now</h1>
                 <p className='text-center text-slate-50 text-2xl font-semibold'>choose your sutable graze box</p>
             </div>
+            <div className='flex container pt-8 gap-4'>
+                <div className='grow'>
+                <button className='text-white bg-green-500 hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-3xl text-sm px-5 py-2.5 text-center'>categoties</button>
+                </div>
+                <button className='text-white bg-green-500 hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-3xl text-sm px-5 py-2.5 text-center'>sort by</button>
+                <button className='text-white text-start bg-green-500 hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-3xl text-sm px-5 py-2.5 '>filter</button>
+            </div>
             <section className='grid grid-cols-4 gap-5 container py-8'>
                 <ProductCard name="mince pie flapjack multipack" price="599" picture={PunnetFlapjackLimitedEdition} />
                 <ProductCard name="mince pie flapjack multipack" price="599" picture={PunnetFlapjackLimitedEdition} />
