85e513a9429133395918feea3e1f0242db256e1d
mohammadHossein712 <mohammadhossdinahmadian@gmail.com>
2023-02-26 19:47:53 +0330

    server maked

---
+++ MERN_shop_project/server/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const cors = require("cors");
+const appRootPath = require("app-root-path")
+const mongoose = require('mongoose');
+
+app = express()
+
+require('dotenv').config({
+    path: appRootPath + "/.env"
+})
+
+app.use(cors())
+app.use(express.json({extended: true}))
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/contactsTask').then((res)=>{
+    console.log("DB conected Ok!")
+})
+
+app.post("")
+const PORT = process.env.PORT || 3000
+app.listen(PORT, ()=>{console.log(`server is run at port ${PORT}`)})
